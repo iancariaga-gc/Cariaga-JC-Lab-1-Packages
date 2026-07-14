01b16398d1a9cbcd165e12f963bed6296711e70a
Ian Jerome Cariaga <ianjerome.cariaga@gcash.com>
2026-07-14 11:03:48 +0800

    finish lab activity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import packageone.ClassOne;
+import packagetwo.ClassTwo;
+import packagethree.ClassThree;
 
-// Import here manually
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ClassOne classOne = new ClassOne();
+        ClassTwo classTwo = new ClassTwo();
+        ClassThree classThree = new ClassThree();
 
-
+        System.out.println(classOne.greet());
+        System.out.println(classTwo.greet());
+        System.out.println(classThree.greet());
     }
 }
